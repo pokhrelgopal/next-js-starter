3703f8e34638dbe63e42109baee7ae77b1e6113b
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-03-13 21:00:12 +0545

    typo fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <ThemeSwitcher />
+      <div className="absolute top-3 right-3">
+        <ThemeSwitcher />
+      </div>
       <h1 className="text-3xl font-bold">Hello World</h1>
     </>
   );
